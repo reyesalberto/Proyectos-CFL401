59a0642d78ecb56b30f20165132904fd97cd7c79
reyesalberto <50602054+reyesalberto@users.noreply.github.com>
2019-07-01 19:13:50 -0300

    Update ReyesTestArrayList.java
    estoy de vuelta de

--- Reyes-Test-ArrayList/src/reyes/test/arraylist/ReyesTestArrayList.java
+++ Reyes-Test-ArrayList/src/reyes/test/arraylist/ReyesTestArrayList.java
@@ -3,7 +3,7 @@ package reyes.test.arraylist;
 public class ReyesTestArrayList {
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println();
+        System.out.println("hola soy bol");
     }
     
 }
