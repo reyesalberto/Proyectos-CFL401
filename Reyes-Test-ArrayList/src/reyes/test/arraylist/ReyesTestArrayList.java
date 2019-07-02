a805e9bad63f1ddbea8219ef3f45dca5f203d5a6
Programación <Programacin@DESKTOP-0JA3C8C>
2019-07-01 21:29:34 -0300

    Test-ArrayList:Inicio de proyecto SOLO

--- Reyes-Test-ArrayList/src/reyes/test/arraylist/ReyesTestArrayList.java
+++ Reyes-Test-ArrayList/src/reyes/test/arraylist/ReyesTestArrayList.java
@@ -1,9 +1,102 @@
-System.out
+
 package reyes.test.arraylist;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
 public class ReyesTestArrayList {
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println();
+        int mascotas;
+        int dato;
+        boolean bandera=true;
+        Scanner teclado = new Scanner(System.in);   
+        dato = teclado.nextint();
+        System.out.println("hola bay bay");
+        
+        ArrayList teclado;
+        teclado = new ArrayList((Collection) System.in);
+        ArrayList elem = new ArrayList();
+        ArrayList <Integer>elem2;
+        elem2 = new ArrayList();
+        elem.add("Perro");
+        elem.add(4);
+        elem.add(7);
+        
+     while(bandera){
+         if(<=4){
+             dato=Scanner(System.in);
+             dato=teclado.netInt();
+         }
+     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      /*  
+      while(bandera)  {
+          dato=teclado.nextLine();
+      }*/
+     /*dato=teclado.nextLine();
+      try{
+          dato=null;
+      }
+          dato=teclado.nextLine();
+          System.out.println(dato);
+      }catch(Exception e){
+          System.out.println(e);
+      }if(dato=null){
+    elem2.add(dato);
+}
+   
     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println(elem); 
+        
+        
+        
+        
+    /* while(bandera){
+         numero = teclado.nextInt();
+             if(>=0){
+                 elem2.add(numero);
+    
+            babdera=false;
+    
+        }else{
+              babdera=false;
+              System.out.println(elem2);
+     }
+    System.out.println("es mayor a "+ numero);
+     
+    
+        }       
+    
     
 }
+  }
+/*dato=teclado.nextLine();
+//while(dato>=0){
+
+elem2.add(dato);
+dato=teclado.netLINE()
+}
+System.out.println(elem2);
+*/
