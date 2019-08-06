7ace279ee964c4f75816893a5b157746d2488825
Programación <Programacin@DESKTOP-0JA3C8C>
2019-08-05 21:30:59 -0300

    reyes20190805j042

---
+++ reyes20190805j042/src/reyes20190805j042/Mascota.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reyes20190805j042;
+
+/**
+ *
+ * @author Programación
+ */
+public class Mascota {
+    private String nombre;
+    private int edad;
+    private int patas;
+    private String tipo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
---
+++ reyes20190805j042/src/reyes20190805j042/Persona.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reyes20190805j042;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Programación
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int dni;
+    private ArrayList <Mascota> mascotas;
+    
+public boolean SosMayorA(int:edad){
+    
+    this.SosMayorA(edad);
+}    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    
+}
